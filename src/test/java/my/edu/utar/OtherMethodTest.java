fa96b36efc315ec17fda254d91a97de1f0802774
wanching_97 <wanching_97@hotmail.com>
2018-03-27 20:11:04 +0800

    added GetSubstringTest

--- src/test/java/my/edu/utar/OtherMethodTest.java
+++ src/test/java/my/edu/utar/OtherMethodTest.java
@@ -29,6 +29,7 @@ public class OtherMethodTest {
         om.countWordInString("donkey", null);
     }
 
+
     @Test
     @Parameters(method = "LargestNumberParams")
     public void testLargestNumberValidValues(int[] numbers, int expectedResult) {
@@ -46,6 +47,28 @@ public class OtherMethodTest {
         om.findLargestNumberInArray(new int[0]);
     }
 
+
+    @Test
+    @Parameters(method = "getParamsForTestGetSubStringsValid")
+    public void testGetSubStringsValid(String[] strArray, int[] strPos, String expectedResult) {
+        String result = om.getSubStrings(strArray, strPos);
+        assertEquals(expectedResult, result);
+    }
+
+    // There are two possible tests for null for either one of the arguments
+    // We do not use parameterized tests as it is not possible to use NULL values
+    // to parameters. Instead we write separate tests.
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetSubStringsInvalidV1() {
+        om.getSubStrings(null, new int[] {2,3});
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testGetSubStringsInvalidV2() {
+        om.getSubStrings(new String[] {"dog", "cat"}, null);
+    }
+
+
     public Object[] CountWordsParams() {
         return new Object[] {
                 new Object[] {"cat dog cat", "cat", 2},
@@ -59,6 +82,22 @@ public class OtherMethodTest {
         return new Object[] {
                 new Object[] {new int[] {4,5,6,7}, 7},
                 new Object[] {new int[]{3}, 3}
+        };
+    }
+
+    private Object[] getParamsForTestGetSubStringsValid() {
+
+        return new Object[] {
+                new Object[] {new String[] {"cat dog mouse", "house fish",
+                        "rendang satay curry mee laksa"},
+                        new int[] {2, 0, 3}, "mouse house mee"},
+                new Object[] {new String[] {"cat dog mouse", "house fish", null,
+                        "rendang satay curry mee laksa"},
+                        new int[] {2, 0, 2, 3}, "mouse house mee"},
+                new Object[] {new String[] {"cat dog mouse", "house fish", "rendang satay" },
+                        new int[] {2, 5, 1}, "mouse satay"},
+                new Object[] {new String[] {"", "", "" }, new int[] {3, 5, 2}, "" },
+                new Object[] {new String[] {}, new int[] {}, ""}
         };
     }
 }
